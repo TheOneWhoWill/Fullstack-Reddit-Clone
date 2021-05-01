be9a2bc3c7f716e9be057a22884bdb9b687165c6
Cybercrafter345 <49823186+Cybercrafter345@users.noreply.github.com>
2021-05-01 15:52:13 -0400

    Add a 'Create Post' bar on top of Posts

--- client/src/components/Posts.js
+++ client/src/components/Posts.js
@@ -1,13 +1,18 @@
+import { faPoll, faLink } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { useAuth } from '../contexts/AuthContext';
 import React, { useEffect, useState } from'react';
+import { useHistory } from 'react-router-dom';
 import axios from 'axios';
 import Post from './Post';
 
 const Posts = React.memo(() => {
 
+  const history = useHistory();
   const { currentUser } = useAuth();
   const [posts, setPosts] = useState([]);
 
+  const profilePicture = currentUser ? currentUser.photoURL : null;
   const query = currentUser ? `http://localhost:2000/posts/user/feed/${currentUser.uid}` : `http://localhost:2000/posts`
 
   useEffect(() => {
@@ -22,6 +27,28 @@ const Posts = React.memo(() => {
 
   return (
     <div className="Posts">
+      <div className="Post CreatePost">
+        <img
+          onClick={() => history.push('/profile')}
+          src={profilePicture}
+          alt="profile"
+        />
+        <input
+          onClick={() => history.push('/create/post')}
+          type="text"
+          placeholder="Create Post"
+        />
+        <FontAwesomeIcon
+          onClick={() => history.push('/create/post')}
+          className="CreatePostIcon"
+          icon={faPoll}
+        />
+        <FontAwesomeIcon
+          onClick={() => history.push('/create/post')}
+          className="CreatePostIcon"
+          icon={faLink}
+        />
+      </div>
       {posts.map(post => {
         return <Post post={post} key={post._id} />
       })}
